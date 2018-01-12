1b5f09ef7d37aac4b25d6c1ef4b128e335ca063f
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-12 19:34:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musician, instrument) {
+  var array = [];
+  for (var i=0; i<musician.length; i++) {
+    array.push(`${musician[i]} plays ${instrument[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var i = facts.length;
+  while(i>0) {
+    facts[i-1].push(!!!);
+    ++i;
+  }
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push(`I love the Beatles!`);
+    ++number;
+  } while (number<15);
+  return array;
+}
