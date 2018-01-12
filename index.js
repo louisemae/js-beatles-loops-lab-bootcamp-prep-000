de374e36f7e6a115b7aa97bbcf69967072a6d754
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-12 19:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function theBeatlesPlay(musician, instrument) {
 }
 
 function johnLennonFacts(facts) {
-  var i = facts.length;
-  while(i>0) {
-    facts[i-1] = `${facts[i-1]}!!!`;
+  var i = 0;
+  while(facts.length>i) {
+    facts[i] = `${facts[i-1]}!!!`;
     ++i;
   }
 }
